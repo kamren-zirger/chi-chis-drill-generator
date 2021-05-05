3893e95ea83e4c9f9d39d0fd3d23b1cc62ca1d35
Kamren Zirger <>
2021-05-05 22:16:35 +0000

    task: adding some fundamentals

---
+++ script.js
@@ -0,0 +1,68 @@
+/*
+ * Chi Chi's Drill Generator
+ * Author: K. Zirger
+ * /
+
+/* Marching Fundamentals */
+
+let fundamentals = {
+  chair: {
+    eightToFive: {
+      forward: {
+        name: "eight forward, 8-to-5",
+        forward: 8,
+        side: 0,
+        counts: 8,
+        step: 8,
+        facing: 0,
+      },
+      backward: {
+        name: "eight backward, 8-to-5",
+        forward: -8,
+        side: 0,
+        counts: 8,
+        step: 8,
+        facing: 0,
+      },
+      rightOblique: {
+        name: "right oblique, 8-to-5",
+        forward: 8,
+        side: 8,
+        counts: 12,
+        step: 8,
+        facing: 0,
+      },
+      leftOblique: {
+        name: "left oblique, 8-to-5",
+        forward: 8,
+        side: -8,
+        counts: 12,
+        step: 8,
+        facing: 0,
+      },
+      rightHornSlide: {
+        name: "eight right horn slide",
+        forward: 8,
+        side: 0,
+        counts: 8,
+        step: 8,
+        facing: 0,
+      },
+      leftHornSlide: {
+        name: "eight left horn slide",
+        forward: 8,
+        side: 0,
+        counts: 8,
+        step: 8,
+        facing: 0,
+      }
+
+    }
+  }
+}
+
+/* Drill Patterns */
+
+let patterns = [
+
+]
