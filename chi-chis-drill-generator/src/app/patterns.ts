65b15e59cc5733779d756846e42b511236bb2821
Kamren <kryptokam@gmail.com>
2021-07-12 22:45:27 -0400

    task: create patterns.ts, make one

---
+++ chi-chis-drill-generator/src/app/patterns.ts
@@ -0,0 +1,17 @@
+/* Chi Chi's Drill Generator */
+
+/* Author: K. Zirger */
+
+import { Fundamental, fundamentals} from "./fundamentals";
+
+export interface Pattern {
+  id: number;
+  fundamentals: Array<Fundamental>;
+}
+
+export const patterns = [
+  {
+    id: 0,
+    fundamentals: [fundamentals[0], fundamentals[1], fundamentals[2]],
+  },
+];
