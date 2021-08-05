b6ca2bf7a326a59d1611e823968113186ccd947a
Kamren <kryptokam@gmail.com>
2021-08-04 23:37:46 -0400

    task: import material / add button

--- chi-chis-drill-generator/src/app/app.module.ts
+++ chi-chis-drill-generator/src/app/app.module.ts
@@ -3,16 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule, MatButtonModule],
+  exports: [MatButtonModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
