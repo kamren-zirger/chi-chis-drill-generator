a7061b4a503f4f60b8559361cd675544d167ac72
Kamren <kryptokam@gmail.com>
2021-08-08 14:36:29 -0400

    task: min length + random pattern add

--- chi-chis-drill-generator/src/app/app.component.ts
+++ chi-chis-drill-generator/src/app/app.component.ts
@@ -8,6 +8,14 @@ function randInt(len: number) {
   return Math.floor(Math.random() * len);
 }
 
+function countSum(a: Array<Fundamental>) {
+  let sum = 0;
+  a.forEach((fund) => {
+    sum += fund.counts;
+  });
+  return sum;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -16,14 +24,29 @@ function randInt(len: number) {
 export class AppComponent {
   title = 'chi-chis-drill-generator';
 
+  minCountNum = 32;
   currentDrill = new Array<Fundamental>();
 
   get pattern() {
     return patterns[randInt(patterns.length)];
   }
 
+  setCountNum(event: any) {
+    this.minCountNum = event.value;
+  }
+
   genDrill() {
+    // Clear old drill
     this.currentDrill = [];
-    this.currentDrill.push(patterns[0].fundamentals[0]);
+
+    // Get random drill until minimum counts satisfied
+    while (countSum(this.currentDrill) < this.minCountNum) {
+      // Get pattern to add
+      let curPattern = patterns[randInt(patterns.length)];
+      // Add each fundamental to drill
+      curPattern.fundamentals.forEach((fund) => {
+        this.currentDrill.push(fund);
+      });
+    }
   }
 }
