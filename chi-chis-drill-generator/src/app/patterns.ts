c727fed84f08f6c1752229f5ef35560ca34a52eb
Kamren <kryptokam@gmail.com>
2021-08-13 22:26:17 -0400

    task: add more patterns

--- chi-chis-drill-generator/src/app/patterns.ts
+++ chi-chis-drill-generator/src/app/patterns.ts
@@ -14,7 +14,7 @@ export interface Pattern {
 export const patterns = [
   {
     id: 0,
-    fundamentals: [fundamentals[0], fundamentals[1], fundamentals[2]],
+    fundamentals: [fundamentals[0], fundamentals[1]],
   },
   {
     id: 1,
@@ -32,5 +32,152 @@ export const patterns = [
       fundamentals[0],
     ],
     name: 'Box of rights',
+  },
+  {
+    id: 3,
+    fundamentals: [
+      fundamentals[0],
+      fundamentals[6],
+      fundamentals[0],
+      fundamentals[6],
+      fundamentals[0],
+      fundamentals[6],
+      fundamentals[0],
+    ],
+    name: 'Box of lefts',
+  },
+  {
+    id: 4,
+    fundamentals: [
+      fundamentals[0],
+      fundamentals[7],
+      fundamentals[0],
+      fundamentals[7],
+      fundamentals[0],
+      fundamentals[7],
+      fundamentals[0],
+      fundamentals[7],
+    ],
+    name: 'Box of rights, right flank',
+  },
+  {
+    id: 5,
+    fundamentals: [
+      fundamentals[0],
+      fundamentals[6],
+      fundamentals[0],
+      fundamentals[6],
+      fundamentals[0],
+      fundamentals[6],
+      fundamentals[0],
+      fundamentals[6],
+    ],
+    name: 'Box of lefts, left flank',
+  },
+  {
+    id: 6,
+    fundamentals: [
+      fundamentals[2],
+      fundamentals[1],
+      fundamentals[3],
+      fundamentals[1],
+    ],
+  },
+  {
+    id: 7,
+    fundamentals: [
+      fundamentals[25],
+      fundamentals[1],
+      fundamentals[26],
+      fundamentals[1],
+    ],
+  },
+  {
+    id: 8,
+    fundamentals: [
+      fundamentals[3],
+      fundamentals[1],
+      fundamentals[2],
+      fundamentals[1],
+    ],
+  },
+  {
+    id: 9,
+    fundamentals: [
+      fundamentals[26],
+      fundamentals[1],
+      fundamentals[25],
+      fundamentals[1],
+    ],
+  },
+  {
+    id: 10,
+    fundamentals: [
+      fundamentals[23],
+      fundamentals[0],
+      fundamentals[27],
+      fundamentals[28],
+    ],
+  },
+  {
+    id: 10,
+    fundamentals: [
+      fundamentals[23],
+      fundamentals[0],
+      fundamentals[28],
+      fundamentals[29],
+    ],
+  },
+  {
+    id: 11,
+    fundamentals: [fundamentals[20]],
+  },
+  {
+    id: 12,
+    fundamentals: [fundamentals[20], fundamentals[20]],
+  },
+  {
+    id: 13,
+    fundamentals: [
+      fundamentals[0],
+      fundamentals[14],
+      fundamentals[13],
+      fundamentals[13],
+      fundamentals[14],
+      fundamentals[1],
+    ],
+  },
+  {
+    id: 14,
+    fundamentals: [
+      fundamentals[0],
+      fundamentals[13],
+      fundamentals[14],
+      fundamentals[14],
+      fundamentals[13],
+      fundamentals[1],
+    ],
+  },
+  {
+    id: 15,
+    fundamentals: [
+      fundamentals[0],
+      fundamentals[14],
+      fundamentals[15],
+      fundamentals[15],
+      fundamentals[14],
+      fundamentals[1],
+    ],
+  },
+  {
+    id: 16,
+    fundamentals: [
+      fundamentals[0],
+      fundamentals[13],
+      fundamentals[16],
+      fundamentals[16],
+      fundamentals[13],
+      fundamentals[1],
+    ],
   },
 ];
